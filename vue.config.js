16ff9a6234c5fcb3312b621ede789104e9e843ec
Jean Joulia <jjeeaann2015@gmail.com>
2022-04-17 21:43:47 +0100

    test

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@
 const path = require("path");
 
 module.exports = {
-  outputDir: path.resolve(__dirname, "./docs")
+  outputDir: path.resolve(__dirname, "./docs"),
+  publicPath: '/proceduralflower/'
 }
